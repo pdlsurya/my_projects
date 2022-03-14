af55f89c3aa36f131568213a00954151997beea6
Surya Poudel <pdlsurya@Suryas-MBP.domain.name>
2022-03-14 20:05:02 +0545

    softTimer updated

--- softTimer/softTimer.h
+++ softTimer/softTimer.h
@@ -1,22 +1,22 @@
-#ifndef __MY_TIMER_H
-#define __MY_TIMER_H
+#ifndef __SOFT_TIMER_H
+#define __SOFT_TIMER_H
 
 /* Macro to define  timer instance */
-#define MY_TIMER_DEF(name)\
+#define SOFT_TIMER_DEF(name)\
 	static softTimer_node_t name;
-#define MS_TO_TICKS(ms) (uint32_t)((uint64_t)ms*32768/1000)   //Macro to convert milliseconds into number of RTC ticks
+#define SOFT_TO_TICKS(ms) (uint32_t)((uint64_t)ms*32768/1000)   //Macro to convert milliseconds into number of RTC ticks
 
 typedef enum {
-	MY_TIMER_MODE_SINGLE_SHOT,
-	MY_TIMER_MODE_REPEATED	
-}my_timer_mode_t;
+	SOFT_TIMER_MODE_SINGLE_SHOT,
+	SOFT_TIMER_MODE_REPEATED	
+}soft_timer_mode_t;
 
 typedef void (*timeout_handler_t)(void);   //Timeout handler function
 
 typedef struct timer_node{
 	bool trig;
 	bool is_running;
-	my_timer_mode_t mode;
+	soft_timer_mode_t mode;
 	timeout_handler_t timeout_handler;
 	uint32_t interval;
 	uint32_t trig_ticks;
@@ -24,7 +24,7 @@ typedef struct timer_node{
 }softTimer_node_t;         // Timer instance structure
 
 //Function to create a timer by initializing the timer instance
-void softTimer_create(softTimer_node_t *instance, uint32_t interval,timeout_handler_t timeout_handler, my_timer_mode_t mode );
+void softTimer_create(softTimer_node_t *instance, uint32_t interval,timeout_handler_t timeout_handler, soft_timer_mode_t mode );
 
 //Function to start the timer instance by adding it to the list of running timers.
 void softTimer_start(softTimer_node_t * instance);
@@ -35,6 +35,6 @@ void softTimer_stop(softTimer_node_t *instance);
 //Function to initialize the timer module
 void softTimer_init();
 
-#endif //__MY_TIMER_H
+#endif //__SOFT_TIMER_H
 
 
