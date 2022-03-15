a1a86f7cfa82ad9c8eef9bd347661545d4a8cb6c
Surya Poudel <pdlsurya@Suryas-MBP.domain.name>
2022-03-15 18:29:52 +0545

    Software I2C updated

--- software_I2C/software_I2C.h
+++ software_I2C/software_I2C.h
@@ -2,8 +2,10 @@
 #define SOFTWARE_I2C_H
 
 
+
 void i2c_init(uint8_t sda, uint8_t scl); //initialize i2c bus
-void i2c_start(uint8_t address);
+void i2c_start(uint8_t address); //Start I2C transmission
+
 bool i2c_write(uint8_t data);
 void i2c_stop();
 
