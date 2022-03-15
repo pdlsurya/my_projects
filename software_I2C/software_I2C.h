54e0cd4ce7533883b47d134e0405150b5729398d
pdlsurya <65765804+pdlsurya@users.noreply.github.com>
2022-03-15 18:27:30 +0545

    Update software_I2C.h

--- software_I2C/software_I2C.h
+++ software_I2C/software_I2C.h
@@ -2,7 +2,7 @@
 #define SOFTWARE_I2C_H
 
 
-void i2c_init(uint8_t sda, uint8_t scl);
+void i2c_init(uint8_t sda, uint8_t scl); //initialize i2c bus
 void i2c_start(uint8_t address);
 bool i2c_write(uint8_t data);
 void i2c_stop();
