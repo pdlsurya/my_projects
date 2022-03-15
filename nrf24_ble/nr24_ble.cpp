cf99bf9115c7115b15376bfc2dff20cf9160f27f
Surya Poudel <pdlsurya@Suryas-MBP.domain.name>
2022-03-15 11:27:54 +0545

    local change

--- nrf24_ble/nr24_ble.cpp
+++ nrf24_ble/nr24_ble.cpp
@@ -78,7 +78,7 @@ void NRF24_BLE::begin(ble_config_t* config_data)
   
 }
 
-void NRF24_BLE::hop_channel()
+void NRF24_BLE::hop_channel() //Frequency hopping spread spectrum
 {
 	ble_instance.current_freq_index++;
 	if(ble_instance.current_freq_index>=sizeof(channel))
